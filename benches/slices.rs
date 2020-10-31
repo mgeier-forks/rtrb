e8020f4f760281bffc96aa26503e93a6565e73df
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-31 14:45:30 +0100

    Add push_slices benchmark

--- benches/slices.rs
+++ benches/slices.rs
@@ -30,6 +30,20 @@ pub fn slices(criterion: &mut criterion::Criterion) {
             i = i.wrapping_add(black_box(1));
         })
     });
+
+    criterion.bench_function("push-single-element-slices", |b| {
+        let (mut p, mut c) = RingBuffer::<u8>::new(1).split();
+        let mut i = 0;
+        b.iter(|| {
+            if let Ok(slices) = p.push_slices(1) {
+                slices.first[0] = black_box(i);
+            } else {
+                unreachable!();
+            }
+            assert_eq!(c.pop(), Ok(black_box(i)));
+            i = i.wrapping_add(black_box(1));
+        })
+    });
 }
 
 criterion_group!(benches, slices);
