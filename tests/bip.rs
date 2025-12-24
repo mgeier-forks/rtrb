7ad3a54d8482f45fb2447e13ac84b28bc65565fd
Matthias Geier <Matthias.Geier@gmail.com>
2025-12-24 15:32:52 +0100

    test: check actual values, clean up code

--- tests/bip.rs
+++ tests/bip.rs
@@ -4,16 +4,8 @@
 
 use rtrb::bip_arc::{Consumer, Producer, RingBuffer};
 
-#[test]
-fn basic() {
-    let (mut p, _c) = RingBuffer::<i32>::new(7);
-    if let Ok(chunk) = p.write_chunk(4) {
-        chunk.commit_all();
-    } else {
-        unreachable!();
-    }
-    assert!(p.write_chunk_uninit(4).is_err());
-}
+type C<'a> = &'a mut Consumer<i32>;
+type P<'a> = &'a mut Producer<i32>;
 
 // Different "slots" functions are tested separately and in all combinations (including no-op)
 // because of caching and potential resetting of `skip`.
@@ -21,53 +13,39 @@ fn basic() {
 #[rstest::rstest]
 fn slots(
     #[values(
-        |p: &mut Producer<i32>, one, two| {
-            assert_eq!(p.slots(), one + two);
-        },
-        |p: &mut Producer<i32>, one, two| {
-            assert_eq!(p.slots_contiguous(), (one, two));
-        },
-        |p: &mut Producer<i32>, one, _| {
-            assert_eq!(p.slots_contiguous_first(), one);
-        },
-        |p: &mut Producer<i32>, one: usize, two| {
-            assert!(p.write_chunk(one.max(two)).is_ok());
-        },
-        |p: &mut Producer<i32>, one: usize, two| {
-            assert!(p.write_chunk(one.max(two) + 1).is_err());
-        },
-        |_: &mut Producer<i32>, _, _| {},
+        |p: P, x, y| assert_eq!(p.slots(), x + y),
+        |p: P, x, y| assert_eq!(p.slots_contiguous(), (x, y)),
+        |p: P, x, _| assert_eq!(p.slots_contiguous_first(), x),
+        |p: P, x: usize, y| assert!(p.write_chunk(x.max(y)).is_ok()),
+        |p: P, x: usize, y| assert!(p.write_chunk(x.max(y) + 1).is_err()),
+        |_: P, _, _| {},
     )]
-    p_slots: fn(&mut Producer<i32>, usize, usize),
+    p_slots: fn(P, usize, usize),
     #[values(
-        |c: &mut Consumer<i32>, one, _| {
-            assert_eq!(c.slots(), one);
-        },
-        |c: &mut Consumer<i32>, one, two| {
-            assert_eq!(c.slots_contiguous(), (one, two));
-        },
-        |c: &mut Consumer<i32>, one, _| {
-            assert_eq!(c.slots_contiguous_first(), one);
-        },
-        |c: &mut Consumer<i32>, one, _| {
-            assert!(c.read_chunk(one).is_ok());
-        },
-        |c: &mut Consumer<i32>, one, _| {
-            assert!(c.read_chunk(one + 1).is_err());
-        },
-        |_: &mut Consumer<i32>, _, _| {},
+        |c: C, x, _| assert_eq!(c.slots(), x),
+        |c: C, x, y| assert_eq!(c.slots_contiguous(), (x, y)),
+        |c: C, x, _| assert_eq!(c.slots_contiguous_first(), x),
+        |c: C, x, _| assert!(c.read_chunk(x).is_ok()),
+        |c: C, x, _| assert!(c.read_chunk(x + 1).is_err()),
+        |_: C, _, _| {},
     )]
-    c_slots: fn(&mut Consumer<i32>, usize, usize),
+    c_slots: fn(C, usize, usize),
     #[values(
-        |p: &mut Producer<i32>, slots| p.write_chunk(slots).unwrap().commit_all(),
-        |p: &mut Producer<i32>, slots| for _ in 0..slots { assert!(p.push(0).is_ok()); },
+        |p: P, vs: &[_]| p.write_chunk(vs.len()).map(|mut ch| {
+            ch.as_mut_slice().copy_from_slice(vs);
+            ch.commit_all();
+        }).unwrap(),
+        |p: P, vs: &[_]| for &v in vs { assert!(p.push(v).is_ok()); },
     )]
-    write_chunk_or_push: fn(p: &mut Producer<i32>, usize),
+    write: fn(P, &[i32]),
     #[values(
-        |c: &mut Consumer<i32>, slots| c.read_chunk(slots).unwrap().commit_all(),
-        |c: &mut Consumer<i32>, slots| for _ in 0..slots { assert!(c.pop().is_ok()); },
+        |c: C, vs: &[_]| c.read_chunk(vs.len()).map(|ch| {
+            assert_eq!(ch.as_slice(), vs);
+            ch.commit_all();
+        }).unwrap(),
+        |c: C, vs: &[_]| for &v in vs { assert_eq!(c.pop(), Ok(v)); },
     )]
-    read_chunk_or_pop: fn(c: &mut Consumer<i32>, usize),
+    read: fn(C, &[i32]),
 ) {
     let (mut p, mut c) = RingBuffer::new(5);
 
@@ -81,31 +59,36 @@ fn slots(
         };
     }
 
-    // w: write index, r: read index, s: skip, x: data, _: empty
+    // w: write index, r: read index, s: skip, _: empty
 
     // ₀_₁_₂_₃_₄_. w=0, r=0, s=_
     assert_slots!(5, 0; 0, 0);
-    write_chunk_or_push(&mut p, 3);
-    // ₀x₁x₂x₃_₄_. w=3, r=0, s=_
+    write(&mut p, &[0, 1, 2]);
+    // ₀0₁1₂2₃_₄_. w=3, r=0, s=_
     assert_slots!(2, 0; 3, 0);
-    read_chunk_or_pop(&mut c, 3);
+    read(&mut c, &[0, 1, 2]);
     // ₀_₁_₂_₃_₄_. w=3, r=3, s=_
     assert_slots!(2, 3; 0, 0);
     // 2 slots are skipped:
-    p.write_chunk(3).unwrap().commit_all();
-    // ₀x₁x₂x₃_₄_. w=3, r=3, s=3
+    p.write_chunk(3)
+        .map(|mut ch| {
+            ch.as_mut_slice().copy_from_slice(&[9, 8, 7]);
+            ch.commit_all();
+        })
+        .unwrap();
+    // ₀9₁8₂7₃_₄_. w=3, r=3, s=3
     // NB: w is allowed to overtake r, because r==s!
     assert_slots!(2, 0; 3, 0);
-    write_chunk_or_push(&mut p, 1);
-    // ₀x₁x₂x₃x₄_. w=4, r=3/0, s=3
+    write(&mut p, &[6]);
+    // ₀9₁8₂7₃6₄_. w=4, r=3/0, s=3
     assert_slots!(1, 0; 4, 0);
-    write_chunk_or_push(&mut p, 1);
-    // ₀x₁x₂x₃x₄x. w=0, r=3/0, s=(3)
+    write(&mut p, &[5]);
+    // ₀9₁8₂7₃6₄5. w=0, r=3/0, s=(3)
 
     //assert_slots!(0, 0; 5, 0);
 
-    //read_chunk_or_pop(&mut c, 4);
-    // ₀_₁_₂_₃_₄x. w=0, r=4, s=_
+    //read(&mut c, &[9, 8, 7, 6]);
+    // ₀_₁_₂_₃_₄5. w=0, r=4, s=_
 
     // TODO: check both cases: (1) write and overtake r (followed by read); (2) read
 }
