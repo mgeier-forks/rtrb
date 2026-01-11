9e5a35ccfcda1091ad88a47d83678e96b5aa959d
Matthias Geier <Matthias.Geier@gmail.com>
2026-01-11 22:49:09 +0100

    Remove now unneeded rustfmt::skip

--- src/bip_arc/producer.rs
+++ src/bip_arc/producer.rs
@@ -457,7 +457,6 @@ impl<T> Producer<T> {
     ///
     /// For a safe alternative that provides
     /// a mutable slice    /// of [`Default`]-initialized slots, see [`Producer::write_chunk()`].
-    #[rustfmt::skip]
     pub fn write_chunk_uninit(&mut self, n: usize) -> Result<WriteChunkUninit<'_, T>, ChunkError> {
         let b = &self.buffer;
         let (mut slots, refreshed_head, try_at_beginning) = self.slots_contiguous_helper();
