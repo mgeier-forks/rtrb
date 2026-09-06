510d1acfa77544925972ea1e3fe1b463c00f1734
Matthias Geier <Matthias.Geier@gmail.com>
2026-09-06 14:53:59 +0200

    use Producer/Consumer::new()

--- src/arc/arc_ring_buffer.rs
+++ src/arc/arc_ring_buffer.rs
@@ -3,7 +3,7 @@
 // using the configuration file `codegen/configs/arc.toml`.
 
 use alloc::boxed::Box;
-use core::{cell::Cell, ptr::NonNull};
+use core::ptr::NonNull;
 
 use super::{Consumer, Producer, RingBuffer};
 
@@ -33,16 +33,10 @@ impl<T> ArcRingBuffer<T> {
         // SAFETY: Pointer from `Box` is always non-null.
         let ptr = unsafe { NonNull::new_unchecked(ptr) };
 
-        let p = Producer {
-            buffer: Self { ptr },
-            cached_head: Cell::new(head),
-            cached_tail: Cell::new(tail),
-        };
-        let c = Consumer {
-            buffer: Self { ptr },
-            cached_head: Cell::new(head),
-            cached_tail: Cell::new(tail),
-        };
+        // SAFETY: `ptr` is valid, `head` and `tail` are correct.
+        let p = unsafe { Producer::new(Self { ptr }, head, tail) };
+        // SAFETY: `ptr` is valid, `head` and `tail` are correct.
+        let c = unsafe { Consumer::new(Self { ptr }, head, tail) };
         (p, c)
     }
 }
